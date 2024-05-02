7ad6390bb1b2601267cd0283639563c9f25f6b74
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-03 00:38:18 +0100

    Refactor log parsing functions and update code in 0x03-log_parsing/0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,41 @@
 #!/usr/bin/python3
-''' stats module '''
 import sys
 import signal
-import re
 
-def signal_handler(signal, frame):
-    ''' signal_handler function '''
+
+def signal_handler(sig, frame):
+    '''Signal handler for SIGINT'''
     print_stats()
     sys.exit(0)
 
 def print_stats():
-    ''' print_stats function '''
-    print('File size: {}'.format(total_size))
-    for key in sorted(status_codes.keys()):
-        if status_codes[key]:
-            print('{}: {}'.format(key, status_codes[key]))
+    '''Prints the stats of the log parsing'''
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
-def parse_line(line):
-    ''' parse_line function '''
-    pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)'  # nopep8
-    match = re.fullmatch(pattern, line.strip())
-    if match:
-        return match.groups()
-    return None, None
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_size = 0
+line_count = 0
 
-def update_stats(status_code, size):
-    ''' update_stats function '''
-    global total_size, status_codes
-    if status_code in status_codes:
-        status_codes[status_code] += 1
-    if size:
-        total_size += int(size)
+signal.signal(signal.SIGINT, signal_handler)
 
-def main():
-    ''' main function '''
-    global line_count
+try:
     for line in sys.stdin:
-        status_code, size = parse_line(line)
-        update_stats(status_code, size)
+        try:
+            data = line.split()
+            size = int(data[-1])
+            status_code = data[-2]
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            total_size += size
+        except:
+            pass
         line_count += 1
         if line_count % 10 == 0:
             print_stats()
-
-if __name__ == '__main__':
-    status_codes = {'200': 0, '301': 0, '400': 0,
-                    '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-    total_size = 0
-    line_count = 0
-    signal.signal(signal.SIGINT, signal_handler)
-    main()
+except KeyboardInterrupt:
+    print_stats()
+    raise
+print_stats()
