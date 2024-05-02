c3c70900d0c12645d6d34fe9d3ec655bc1101761
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-02 23:38:45 +0100

    complete task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,23 +2,13 @@
 ''' stats module '''
 import sys
 import signal
-import re
-# Write a script that reads stdin line by line and computes metrics:
 
-# Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> 
-# (if the format is not this one, the line must be skipped)
-# After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
-# Total file size: File size: <total size>
-# where <total size> is the sum of all previous <file size> (see input format above)
-# Number of lines by status code:
-# possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-# if a status code doesn’t appear or is not an integer, don’t print anything for this status code
-# format: <status code>: <number>
-# status codes should be printed in ascending order
-# Warning: In this sample, you will have random value - it’s normal to not have the same output as this one.
+
 def signal_handler(signal, frame):
-  ''' signal_handler function '''
-  pass
+    ''' signal_handler function '''
+    print_stats()
+    sys.exit(0)
+
 
 def print_stats():
     ''' print_stats function '''
@@ -27,19 +17,33 @@ def print_stats():
         if status_codes[key]:
             print('{}: {}'.format(key, status_codes[key]))
 
+
 def parse_line(line):
-  ''' parse_line function '''
-  
+    ''' parse_line function '''
+    global total_size, line_count, status_codes
+    try:
+        data = line.split()
+        total_size += int(data[-1])
+        status_code = data[-2]
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        line_count += 1
+    except BaseException:
+        pass
 
 
 def main():
-  ''' main function '''
-    
+    ''' main function '''
+    for line in sys.stdin:
+        parse_line(line)
+        if line_count % 10 == 0:
+            print_stats()
 
 
 if __name__ == '__main__':
-  status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-  total_size = 0
-  line_count = 0
-  signal.signal(signal.SIGINT, signal_handler)
-  main()
+    status_codes = {'200': 0, '301': 0, '400': 0,
+                    '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    total_size = 0
+    line_count = 0
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
