30593cb6ad5ab543c82cf6e74d0bc1ea2dd06ef6
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-19 15:26:10 +0100

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,52 +1,44 @@
 #!/usr/bin/python3
-"""the N queens problem.
-"""
-
+"""doc doc doc"""
 import sys
 
-def print_usage_and_exit():
-    print("Usage: nqueens N")
-    sys.exit(1)
 
-def is_safe(board, row, col):
-    """ Check if a queen can be placed on board[row][col]. """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+def solve_queens_problem(N):
+    """doc doc doc"""
 
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-def solve_nqueens(board, col):
-    """ Use backtracking to solve the N queens problem. """
-    if col >= len(board):
-        solution = []
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        print(solution)
+    def is_valid_position(pos, occupied_pos):
+        """doc doc doc"""
+        for i in range(len(occupied_pos)):
+            if (
+                occupied_pos[i] == pos or
+                occupied_pos[i] - i == pos - len(occupied_pos) or
+                occupied_pos[i] + i == pos + len(occupied_pos)
+            ):
+                return False
         return True
 
-    res = False
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            res = solve_nqueens(board, col + 1) or res
-            board[i][col] = 0
+    def place_queens(N, index, occupied_pos, solutions):
+        """doc doc doc"""
+        if index == N:
+            solutions.append(occupied_pos[:])
+            return
 
-    return res
+        for i in range(N):
+            if is_valid_position(i, occupied_pos):
+                occupied_pos.append(i)
+                place_queens(N, index + 1, occupied_pos, solutions)
+                occupied_pos.pop()
+
+    solutions = []
+    place_queens(N, 0, [], solutions)
+    return solutions
+
 
 def main():
+    """doc doc doc"""
     if len(sys.argv) != 2:
-        print_usage_and_exit()
+        print("Usage: nqueens N")
+        sys.exit(1)
 
     try:
         N = int(sys.argv[1])
@@ -58,8 +50,10 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solve_nqueens(board, 0)
+    solutions = solve_queens_problem(N)
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(len(solution))])
+
 
 if __name__ == "__main__":
     main()
