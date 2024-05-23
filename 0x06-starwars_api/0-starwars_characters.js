aad430a555e8895d6cf0de61b1601f7bf87794e9
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-05-23 19:21:31 +0100

    Refactor Star Wars character fetching code to use async/await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
+
 const request = require('request');
-const util = require('util');
-const requestPromise = util.promisify(request);
 const movieId = process.argv[2];
 
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
@@ -9,12 +8,21 @@ const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 request(url, async function (error, response, body) {
   if (error) {
     console.error('error:', error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    const characterPromises = characters.map((characterUrl) => requestPromise(characterUrl));
-    const characterResponses = await Promise.all(characterPromises);
-    characterResponses.forEach((characterResponse) => {
-      console.log(JSON.parse(characterResponse.body).name);
+    return;
+  }
+
+  const characters = JSON.parse(body).characters;
+  for (const characterUrl of characters) {
+    await new Promise((resolve, reject) => {
+      request(characterUrl, function (error, response, body) {
+        if (error) {
+          console.error('error:', error);
+          reject(error);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
     });
   }
 });
